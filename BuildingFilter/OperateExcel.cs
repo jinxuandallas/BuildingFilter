7737c26193b979ae20cf261fdd448c29127de520
Dallas <dallas2k@163.com>
2023-03-27 22:59:36 +0800

    初步完成，还差地址楼宇确定

---
+++ BuildingFilter/OperateExcel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace BuildingFilter
+{
+    internal class OperateExcel
+    {
+        private string excelPath;
+        private Excel.Application? myApp;
+        private object missing = System.Reflection.Missing.Value;
+        public OperateExcel()
+        {
+            excelPath = System.Environment.CurrentDirectory + @"\company.xlsx";
+        }
+
+
+
+
+
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowThreadProcessId(IntPtr hwnd, out int processid);
+        public void OperateContent(List<List<string>> content)
+        {
+            myApp = new Excel.Application();
+            Excel.Workbook workbook = myApp.Workbooks.Open(excelPath);
+            Excel.Worksheet worksheet = (Excel.Worksheet)myApp.Worksheets.Add();
+
+
+            Excel.Range range = (Excel.Range)worksheet.Cells[1, 5];
+            range.HorizontalAlignment = Microsoft.Office.Core.XlHAlign.xlHAlignCenter;
+            range.Value = DateTime.Now.Date.ToString("yyyy年MM月dd日");
+            range.Font.Size = 30;
+
+            int i = 2;
+            foreach (var item in content)
+            {
+                i++;
+                string[] i_arr=item.ToArray();
+                Excel.Range row = worksheet.Range[worksheet.Cells[i, 2], worksheet.Cells[i,2+i_arr.Length-1]];
+                row.Value2 = i_arr;
+            }
+            //StreamReader streamReader = new StreamReader(content);
+
+            //string? line = streamReader.ReadLine();
+            //while (line != null)
+            //{
+            //    if (line.Contains("："))
+            //    {
+
+            //    }
+            //    line = streamReader.ReadLine();
+
+            //}
+
+            //streamReader.Close();
+
+            workbook.Save();
+            workbook.Close();
+
+            myApp.Quit();
+
+            int pid;
+
+            GetWindowThreadProcessId(new IntPtr(myApp.Hwnd), out pid);
+
+            System.Diagnostics.Process.GetProcessById(pid).Kill();
+
+
+            //System.Runtime.InteropServices.Marshal.ReleaseComObject(myApp);
+            //Process[] ExcelProcess = Process.GetProcessesByName("Excel");
+            ////关闭进程
+            //foreach (Process p in ExcelProcess)
+            //{
+            //    p.Kill();
+            //}
+            //myApp = null;
+        }
+
+        public void test()
+        {
+            myApp = new Excel.Application();
+            Excel.Workbook workbook = myApp.Workbooks.Open(excelPath);
+            Excel.Worksheet worksheet = (Excel.Worksheet)myApp.Worksheets.Add();
+
+
+            Excel.Range range = (Excel.Range)worksheet.Cells[1,5];
+            range.HorizontalAlignment = Microsoft.Office.Core.XlHAlign.xlHAlignCenter;
+            range.Value = DateTime.Now.Date.ToString("yyyy年MM月dd日");
+            range.Font.Size = 30;
+            workbook.Save();
+            workbook.Close();
+
+            myApp.Quit();
+            //System.Runtime.InteropServices.Marshal.ReleaseComObject(myApp);
+            int pid;
+
+            GetWindowThreadProcessId(new IntPtr(myApp.Hwnd), out pid);
+
+            System.Diagnostics.Process.GetProcessById(pid).Kill();
+            //PublicMethod.Kill(myApp);
+
+            //Process[] ExcelProcess = Process.GetProcessesByName("Excel");
+            ////关闭进程
+            //foreach (Process p in ExcelProcess)
+            //{
+            //    p.Kill();
+            //}
+            myApp = null;
+
+        }
+
+
+    }
+
+  
+
+
+}
--- BuildingFilter/Program.cs
+++ BuildingFilter/Program.cs
@@ -1,4 +1,83 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BuildingFilter;
 
-Console.ReadLine();
-Console.WriteLine("Hello, World!");
+var compath = @"txt\com.txt";
+var streetpaht = @"txt\街道.txt";
+
+List<string> streets = ReadStreets(streetpaht);
+StreamReader sr = new StreamReader(compath);
+
+string? line = sr.ReadLine();
+bool greater, createDate;
+List<List<string>> content = new List<List<string>>();
+while (line != null)
+{
+    List<string> item = new List<string>();
+
+    if (line.Contains("："))
+    {
+        line = line.Trim('。');//去掉句号
+        greater = createDate = false;
+        string[] single = line.Split('，');
+
+        foreach (string s in single)
+        {
+
+            string[] kv = s.Split('：');
+            if (kv.Length < 2)
+                continue;
+            if (kv[0].Contains("企业注册资本"))
+            {
+                float capital = float.Parse(kv[1]);
+                if (capital < 500)
+                    break;
+                else
+                    greater = true;
+            }
+
+            if (kv[0].Contains("成立日期"))
+                createDate = true;
+
+            item.Add(kv[1].Trim());
+        }
+
+
+
+        if (greater && streets.Contains(item[item.Count - 1]))//在10个街道中
+        {
+            if (!createDate)//如果没有成立日期则填充空白
+                item.Insert(item.Count - 2, "");
+            content.Add(item);
+        }
+
+    }
+    line = sr.ReadLine();
+
+}
+sr.Close();
+//Console.WriteLine(line);
+
+
+Console.WriteLine();
+foreach (string s in streets)
+    Console.WriteLine(s);
+
+OperateExcel oe = new OperateExcel();
+//oe.test();
+oe.OperateContent(content);
+
+
+List<string> ReadStreets(string path)
+{
+    List<string> result = new List<string>();
+    StreamReader sr = new StreamReader(path);
+    string? line = sr.ReadLine();
+    while (line != null)
+    {
+        result.Add(line.Trim() + "街道");
+        line = sr.ReadLine();
+    }
+    sr.Close();
+    return result;
+
+}
